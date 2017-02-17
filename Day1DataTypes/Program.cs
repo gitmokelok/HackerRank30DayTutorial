753c0be94189505e50c1655c8cabac4e04ff789e
DMITRIJ MAMAJEV <mokelok@live.ru>
2017-02-17 21:10:54 +0200

    first commit

--- Day1DataTypes/Program.cs
+++ Day1DataTypes/Program.cs
@@ -10,6 +10,24 @@ namespace Day1DataTypes
     {
         static void Main(string[] args)
         {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            // Declare second integer, double, and String variables.
+            int i1 = 3;
+            double d1 = 2.8;
+            string s1 = "is the best place to learn and practice coding!";
+            // Read and save an integer, double, and String to your variables.
+
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(i + i1);
+            Console.WriteLine($"{d1+d:F1}");
+            Console.WriteLine(s + s1);
+            // Print the sum of the double variables on a new line.
+
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
         }
     }
 }

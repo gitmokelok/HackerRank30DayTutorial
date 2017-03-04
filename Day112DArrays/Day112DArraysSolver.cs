986d52b4767eff5cb988f834e9e557be7633c181
dmitrij.mamajev <dmitrij.mamajev@simplanova.com>
2017-03-04 12:46:32 +0200

    Some code added for solution

--- Day112DArrays/Day112DArraysSolver.cs
+++ Day112DArrays/Day112DArraysSolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,20 @@ namespace Day112DArrays
 {
     public class Day112DArraysSolver
     {
+        public Dictionary<int[],int> ExtractHourGlasses(int[][] arrayOfInts)
+        {
+            var result = new Dictionary<int[], int>(16);
 
+            for (int i = 0; i < arrayOfInts.Length-3; i++)
+            {
+                var hourGlassUpperRow = int[3];
+                for (int j = 0; j < arrayOfInts.Length - 3; j++)
+                {
+                    
+                }
+            }
+
+            return result;
+        }
     }
 }

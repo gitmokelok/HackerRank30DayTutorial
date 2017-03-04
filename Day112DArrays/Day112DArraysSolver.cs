ab096229e466c847cd2bd9c0a26c127fa419bf4b
DMITRIJ MAMAJEV <mokelok@live.ru>
2017-03-04 22:04:37 +0200

    updated functionality

--- Day112DArrays/Day112DArraysSolver.cs
+++ Day112DArrays/Day112DArraysSolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,21 @@ namespace Day112DArrays
 
             for (int i = 0; i < arrayOfInts.Length-3; i++)
             {
-                var hourGlassUpperRow = int[3];
-                for (int j = 0; j < arrayOfInts.Length - 3; j++)
+                for (int j = 0; j < arrayOfInts.Length-3; j++) 
                 {
-                    
+                    //var upperRow = new[] {arrayOfInts[i][j], arrayOfInts[i][j + 1], arrayOfInts[i][j + 2]};
+                    //var middleNumber = arrayOfInts[i + 1][ j + 1];
+                    //var lowerRow = new[] {arrayOfInts[i + 2][j], arrayOfInts[i][j + 2], arrayOfInts[i + 2][j + 2]};
+                    var tempArray = new[] {arrayOfInts[i][j], arrayOfInts[i][j+1], arrayOfInts[i][j+2], arrayOfInts[i+1][j+1], arrayOfInts[i+2][j], arrayOfInts[i+2][j+1], arrayOfInts[i+2][j+2]};
+                    var hourGlassSum = 0;
+                    foreach (var number in tempArray)
+                    {
+                        hourGlassSum += number;
+                    }
+
+                    result.Add(tempArray, hourGlassSum);
                 }
+                
             }
 
             return result;
--- Day112DArrays/Program.cs
+++ Day112DArrays/Program.cs
@@ -10,12 +10,30 @@ namespace Day112DArrays
     {
         static void Main(string[] args)
         {
-            int[][] arr = new int[6][];
-            for (int arr_i = 0; arr_i < 6; arr_i++)
+            var arr = new int[6][];
+            arr[0] = new int[] { 1,1,1,0,0,0};
+            arr[1] = new int[] {0, 2, 0, 0, 0, 0};
+            arr[2] = new int[] {3, 3, 3, 0, 0, 0};
+            arr[3] = new int[] {0, 0,0,0,0,0};
+            arr[4] = new int[] { 0, 0, 0, 0, 0, 0 };
+            arr[5] = new int[] { 0, 0, 0, 0, 0, 0 };
+
+            //for (int arr_i = 0; arr_i < 6; arr_i++)
+            //{
+            //    string[] arr_temp = Console.ReadLine().Split(' ');
+            //    arr[arr_i] = Array.ConvertAll(arr_temp, Int32.Parse);
+            //}
+
+            var solver = new Day112DArraysSolver();
+            var hourGlassCollection = solver.ExtractHourGlasses(arr);
+            var largestSum = -9;
+            foreach (var glass in hourGlassCollection)
             {
-                string[] arr_temp = Console.ReadLine().Split(' ');
-                arr[arr_i] = Array.ConvertAll(arr_temp, Int32.Parse);
+                if (glass.Value > largestSum)
+                    largestSum = glass.Value;
             }
+            Console.WriteLine(largestSum);
+
         }
     }
 }

12868efffdc5c77f403ceb9cca26d724e39c5eec
dmitrij.mamajev <dmitrij.mamajev@simplanova.com>
2017-03-04 12:18:20 +0200

    Added excercise Main code for reading input

--- Day112DArrays/Program.cs
+++ Day112DArrays/Program.cs
@@ -10,6 +10,12 @@ namespace Day112DArrays
     {
         static void Main(string[] args)
         {
+            int[][] arr = new int[6][];
+            for (int arr_i = 0; arr_i < 6; arr_i++)
+            {
+                string[] arr_temp = Console.ReadLine().Split(' ');
+                arr[arr_i] = Array.ConvertAll(arr_temp, Int32.Parse);
+            }
         }
     }
 }

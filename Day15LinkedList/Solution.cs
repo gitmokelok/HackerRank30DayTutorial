2029f5ddc2738e19ed7990fee865eab1bdecc54e
DMITRIJ MAMAJEV <mokelok@live.ru>
2017-03-10 23:07:25 +0200

    Added day15 linkedlist project and part solution

---
+++ Day15LinkedList/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15LinkedList
+{
+    class Solution
+    {
+        public static Node insert(Node head, int data)
+        {
+            //Complete this method
+            Node currentNode = head;
+            while (currentNode.next == null)
+                currentNode = currentNode.next;
+            currentNode.data = data;
+            return currentNode;
+
+        }
+        public static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Node head = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            display(head);
+        }
+    }
+
+
+    class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+    }
+}

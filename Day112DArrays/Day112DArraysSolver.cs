0ae69fe120d6c50ca2bfe77dba1362ae5981e190
DMITRIJ MAMAJEV <mokelok@live.ru>
2017-03-10 21:55:29 +0200

    cleanup

--- Day112DArrays/Day112DArraysSolver.cs
+++ Day112DArrays/Day112DArraysSolver.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.CodeDom;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Day112DArrays
 {

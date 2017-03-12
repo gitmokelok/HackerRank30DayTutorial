707fc1aada4870c48c89806cae67f716c4fd33ec
DMITRIJ MAMAJEV <mokelok@live.ru>
2017-03-12 19:35:37 +0200

    Day 16 Exceptions Handling

---
+++ Day16 Exceptions Handling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_Exceptions_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                int convertedString = int.Parse(S);
+                Console.WriteLine(convertedString);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bad String");
+            }
+
+        }
+    }
+}

30092c7598d492d790330cc0b9153cce70a6d4aa
DMITRIJ MAMAJEV <mokelok@live.ru>
2017-03-13 23:16:12 +0200

    Day21 Generics solution and project added

---
+++ Day21Generics/Printer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21Generics
+{
+    public class Printer
+    {
+        public static void PrintArray<T>(IList<T> array)
+        {
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}

2d9d889d1b672749d2a630b5eaaf848d19e49b45
DMITRIJ MAMAJEV <mokelok@live.ru>
2017-03-17 20:19:32 +0200

    Day 21 BST height calculation

---
+++ Day22BinaryTree/BinaryTree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22BinaryTree
+{
+    public class BinaryTree
+    {
+        public int getHeight(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            return (-1 + Math.Max(getHeight(root.left), getHeight(root.right)));
+
+
+
+        }
+
+        public Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+    }
+
+    public class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+}
